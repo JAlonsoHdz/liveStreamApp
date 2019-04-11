4d922f6f1b1f8ec4f0d0396c6d77c451c98b85ad
Alonso Hernandez <jesus.alonso.hernandez@gmail.com>
2019-04-11 16:05:43 -0500

    added Header and included libraries

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import StreamCreate from './StreamCreate';
 import StreamDelete from './StreamDelete';
 import StreamEdits from './StreamEdits';
 import StreamList from './StreamList';
 import StreamShow from './StreamShow';
+import Header from './Header';
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <div>
-                <Route path="/" exact component={StreamList} /> 
-                <Route path="/create" exact component={StreamCreate} />
-                <Route path="/delete" exact component={StreamDelete} />
-                <Route path="/edits" exact component={StreamEdits} />              
-                <Route path="/show" exact component={StreamShow} />
-            </div>
-            <div>Hello</div>
-        </BrowserRouter>
+        <div>
+            <Header />
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={StreamList} /> 
+                    <Route path="/create" exact component={StreamCreate} />
+                    <Route path="/delete" exact component={StreamDelete} />
+                    <Route path="/edits" exact component={StreamEdits} />              
+                    <Route path="/show" exact component={StreamShow} />
+                </div>
+                
+            </BrowserRouter>
+        </div>
         
     );
 };
